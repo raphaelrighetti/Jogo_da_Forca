979ea04ca9b3687863bdc12fcd88aa8921408023
Raphael Righetti <raphaelrighettiramos@hotmail.com>
2022-12-08 01:04:35 -0300

    update

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,10 +1,16 @@
 import random
+import os
+import platform
+from time import sleep
 
 palavras = ('amor', 'odio', 'felicidade', 'sentimento',
-            'lobo', 'macaco', 'morango', 'pote')
+            'lobo', 'macaco', 'morango', 'pote',
+            'sofrimento', 'saudade', 'jesus', 'diabo')
 
 palavra_secreta = random.choice(palavras)
 palavra_formatada_arr = []
+letras_acertadas = []
+letras_erradas = []
 
 tentativas = 10
 
@@ -13,7 +19,6 @@ for l in palavra_secreta:
 
 dificuldade = int(input(
     'Escolha uma dificuldade de um a 3, onde 1 é a mais baixa e 3 a mais alta: '))
-
 
 if dificuldade > 3 or dificuldade < 1:
     print('O número passado não estava dentro dos padrões, seguindo na dificuldade 2.')
@@ -25,24 +30,56 @@ elif dificuldade == 2:
 elif dificuldade == 3:
     tentativas = 7
 
+os.system('cls' if platform.system() == 'Windows' else 'clear')
+
 print('Para sair, digite "sair" ou aperte CTRL + C')
 
 while True:
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+    print(f'Tentativas restantes: {tentativas}')
+    print(''.join(palavra_formatada_arr))
+
     if tentativas < 1:
+        os.system('cls' if platform.system() == 'Windows' else 'clear')
         print('Você perdeu!')
+        sleep(5)
         break
 
     letra = input('Digite uma letra: ')
-    index = 0
 
     if letra.lower() == 'sair':
         break
     elif len(letra) > 1:
         print('Digite apenas uma letra!')
+        sleep(1.5)
         continue
     elif not letra.isalpha():
         print('O script só aceita letras!')
+        sleep(1.5)
         continue
+    elif letra in letras_acertadas:
+        print('Você já chutou essa letra.')
+        sleep(1.5)
+        continue
+    elif letra in letras_erradas:
+
+        tentativas -= 1
+        print('Você já chutou essa letra (e errou).')
+        sleep(1.5)
+        continue
+
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+
+    if letra in palavra_secreta:
+        letras_acertadas.append(letra)
+    else:
+        letras_erradas.append(letra)
+
+    acertou = letra in palavra_secreta
+    index = 0
+
+    if not acertou:
+        tentativas -= 1
 
     while index < len(palavra_formatada_arr):
         if palavra_formatada_arr[index] != '*':
@@ -52,11 +89,8 @@ while True:
             palavra_formatada_arr[index] = letra
         index += 1
 
-    tentativas -= 1
-
-    print(f'Tentativas restantes: {tentativas}')
-    print(''.join(palavra_formatada_arr))
-
     if ''.join(palavra_formatada_arr) == palavra_secreta:
+        os.system('cls' if platform.system() == 'Windows' else 'clear')
         print('Você venceu!')
+        sleep(5)
         break
